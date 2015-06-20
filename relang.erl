4cc7d2eaa225c8821668dd9c94cc4b3c0abf54b6
Vinh <vinh@listia.com>
2015-06-20 14:06:55 -0700

    Finally made open,handshake work without authkey

--- relang.erl
+++ relang.erl
@@ -13,7 +13,6 @@
 connect(RethinkDBHost) ->
   {ok, Sock} = gen_tcp:connect(RethinkDBHost, 28015,
                                [binary, {packet, 0}, {active, false}]),
-  ok = gen_tcp:send(Sock, "Some Data"),
   Sock.
 
 close(Sock) ->
@@ -21,8 +20,13 @@ close(Sock) ->
 
 handshake(Sock, AuthKey) ->
   KeyLength = iolist_size(AuthKey),
-  ok = gen_tcp:send(Sock, binary:encode_unsigned(1063369270, little)),
-  ok = gen_tcp:send(Sock, [<<KeyLength:32/little-unsigned>>, AuthKey]),
+  ok = gen_tcp:send(Sock, binary:encode_unsigned(16#400c2d20, little)),
+  %%ok = gen_tcp:send(Sock, [<<KeyLength:32/little-unsigned>>, AuthKey]),
+  ok = gen_tcp:send(Sock, [<<0:32/little-unsigned>>]),
+  %%ok = gen_tcp:send(Sock, binary:encode_unsigned(16#0)),
+  %%ok = gen_tcp:send(Sock, binary:encode_unsigned(16#0)),
+  %%ok = gen_tcp:send(Sock, binary:encode_unsigned(16#7e6970c7)),
+  ok = gen_tcp:send(Sock, [<<16#7e6970c7:32/little-unsigned>>]),
   {ok, Response} = read_until_null(Sock),
   case Response == <<"SUCCESS",0>> of
       true -> ok;
@@ -34,7 +38,7 @@ handshake(Sock, AuthKey) ->
 run() ->
   RethinkDBHost = "127.0.0.1", % to make it runnable on one machine
   RethinkSock   = connect(RethinkDBHost),
-  handshake(RethinkSock, <<>>),
+  handshake(RethinkSock, <<"">>),
   close(RethinkSock).
 
 read_until_null(Socket) ->
