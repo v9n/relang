9f5a24138f94280a1f0d4687ffe0130f8ff9a8b6
Vinh <vinh@listia.com>
2015-07-10 15:46:59 -0700

    Cleanup junk

--- src/relang.erl
+++ src/relang.erl
@@ -3,10 +3,6 @@
 -author(kureikain).
 -email("kurei@axcoto.com").
 
--type document() :: {[keyvalue()]}.
--type keyvalue() :: {binary(), json_term()}.
--type json_term() :: null | boolean() | number() | binary() | document() | [json_term()].
-
 %%-export([connect/1]).
 -compile(export_all). %% replace with -export() later, for God's sake!
 
@@ -27,6 +23,7 @@ stop() ->
 connect(RethinkDBHost) ->
   {ok, Sock} = gen_tcp:connect(RethinkDBHost, 28015,
                                [binary, {packet, 0}, {active, false}]),
+  handshake(Sock, <<"">>),
   Sock.
 
 close(Sock) ->
@@ -37,14 +34,8 @@ handshake(Sock, AuthKey) ->
   ok = gen_tcp:send(Sock, binary:encode_unsigned(16#400c2d20, little)),
   %%ok = gen_tcp:send(Sock, [<<KeyLength:32/little-unsigned>>, AuthKey]),
   ok = gen_tcp:send(Sock, [<<0:32/little-unsigned>>]),
-  %%ok = gen_tcp:send(Sock, binary:encode_unsigned(16#0)),
-  %%ok = gen_tcp:send(Sock, binary:encode_unsigned(16#0)),
-  %%ok = gen_tcp:send(Sock, binary:encode_unsigned(16#7e6970c7)),
-  
-  % Using JSON
+  % Using JSON Protocol
   ok = gen_tcp:send(Sock, [<<16#7e6970c7:32/little-unsigned>>]),
-  %Using protobuf
-  %ok = gen_tcp:send(Sock, [<<16#271ffc41:32/little-unsigned>>]),
 
   {ok, Response} = read_until_null(Sock),
   case Response == <<"SUCCESS",0>> of
@@ -60,51 +51,11 @@ query(Socket, RawQuery) ->
   Token = random:uniform(3709551616),
   io:format("QueryToken = ~p~n", [Token]),
   Query = relang_ast:make(RawQuery),
-
-  %Query = #query {
-  %           type = 'START',
-  %           query = [{db, <<"db">>}, {table_list}],
-  %           token = Token,
-  %           global_optargs = [ #query_assocpair {
-  %                                 key = <<"db">>,
-  %                                 val = #term {
-  %                                          type = 'DB',
-  %                                          args = #term {
-  %                                                    type = 'DATUM',
-  %                                                    datum = #datum {
-  %                                                               type = 'R_STR',
-  %                                                               r_str = <<"test">>
-  %                                                              }
-  %                                                   }
-  %                                         }
-  %                                }]
-  %          },
-  %Iolist2 = ql2_pb:encode_query(Query),
-  Query2  = #query {
-               type = 'START',
-               query = #term {
-                          type = #term {
-                                    type = 'DATUM',
-                                    datum = #datum {
-                                              type = 'R_STR', r_str = <<"DB_LIST">>
-                                              },
-                                    args = []
-                                   },
-                          args = []
-                       },
-               token = Token
-              },
-  %Iolist  = ql2_pb:encode_query({query, 1, {term, <<59>>, undefined, [], undefined}, false,  false, undefined, undefined}),
   io:format("Query = ~p ~n", [Query]),
-  %Iolist  = "[1,[60,[[14,[\"test\"]],\"tv_shows\"]],{}]", create table
   Iolist  = ["[1,["] ++ Query ++ ["],{}]"], % list db 
   Length = iolist_size(Iolist),
   io:format("Query= ~p~n", [Iolist]),
   io:format("Length: ~p ~n", [Length]),
-
-  %ok = gen_tcp:send(Socket, [<<Token:64/little-unsigned>>]),
-  %ok = gen_tcp:send(Socket, [<<Length:32/little-unsigned>>]),
-  %ok = gen_tcp:send(Socket, [Iolist]),
 
   case gen_tcp:send(Socket, [<<Token:64/little-unsigned>>, <<Length:32/little-unsigned>>, Iolist]) of
     ok -> ok;
@@ -123,15 +74,6 @@ query(Socket, RawQuery) ->
       io:fwrite("Got Error when receving: ~s ~n", [ErrReason]),
       ErrReason
   end
-
-  %fmt:fwrite("Result: ~s ~n", [Response]),
-  %{Result, Code} = gen_tcp:send(Socket, [Query]),
-  %case Result of
-  %  ok -> ok;
-  %  error ->
-  %    io:fwrite("Error: ~s ~n", [Code]),
-  %    {error, Code}
-  %end,
   .
 
 
@@ -164,7 +106,6 @@ recv(Socket) ->
 run() ->
   RethinkDBHost = "127.0.0.1", % to make it runnable on one machine
   RethinkSock   = connect(RethinkDBHost),
-  handshake(RethinkSock, <<"">>),
 
   Qlist = [{db_list}],
   Qtlist = [{db, [<<"test">>]}, {table_list}],
